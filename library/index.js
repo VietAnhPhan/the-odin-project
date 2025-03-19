46a32b0ae6ae76a1274b75dfcf4ae82cc835156f
Anh Phan <vietanhphan2810@gmail.com>
2025-03-19 14:23:21 +0000

    clean code

--- library/index.js
+++ library/index.js
@@ -1,20 +1,3 @@
-// const addButton = document.querySelector('button');
-// const authorInput = document.querySelector('#author');
-// const titleInput = document.querySelector('#title');
-// const pagesInput = document.querySelector('#pages');
-// const readInput = document.querySelector('#read');
-// const libraryDiv = document.querySelector('.library');
-// libraryDiv.innerHTML = '';
-
-// addButton.addEventListener('click', addBook);
-
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// }
-
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
@@ -23,10 +6,6 @@ class Book {
         this.read = read;
     }
 }
-
-// function Library() {
-//     this.books = [];
-// }
 
 class Library {
     constructor(books = []) {
@@ -42,100 +21,6 @@ class Library {
     }
 }
 
-// Library.prototype.addBook = function (book) {
-//     this.books.push(book);
-// }
-
-// function displayLibrary() {
-
-//     library.books.forEach((book, index) => {
-//         const bookDiv = document.createElement('div');
-//         bookDiv.classList.add('book');
-//         bookDiv.setAttribute('data-attribute', index);
-//         bookDiv.innerHTML = `<h2>Title: ${book.title}</h2>
-//         <p><em>Author:</em> ${book.author}</p>
-//         <p><em>Pages:</em> ${book.pages}</p>
-//         <p><em>Read:</em> ${book.read ? 'Yes' : 'No'}</p>
-//         <button class="remove" onclick='removeBook(${index})'>Remove</button>
-//         <label>Have you read it?</label>`;
-
-//         const readCheckbox = document.createElement('input');
-
-//         readCheckbox.type = 'checkbox';
-//         readCheckbox.classList.add('read');
-
-//         if (book.read) {
-//             bookDiv.style.backgroundColor = 'lightgreen';
-//             readCheckbox.checked = true;
-//         }
-
-//         readCheckbox.addEventListener('change', () => {
-//             if (readCheckbox.checked) {
-//                 bookDiv.style.backgroundColor = 'lightgreen';
-//             }
-//             else {
-//                 bookDiv.style.backgroundColor = 'white';
-//             }
-
-//         });
-
-//         bookDiv.appendChild(readCheckbox);
-//         libraryDiv.appendChild(bookDiv);
-//     });
-// }
-
-
-
-// function addBook(event) {
-//     const title = titleInput.value == "" ? "The best study skill you ever now" : titleInput.value;
-//     const author = authorInput.value == "" ? "Unknown" : authorInput.value;
-//     const pages = pagesInput.value == "" ? 0 : pagesInput.value;
-//     const read = readInput.checked;
-//     const book = new Book(title, author, pages, read);
-//     library.addBook(book);
-//     const index = library.books.length - 1;
-//     createBookBlock(book, index);
-//     event.preventDefault();
-// }
-
-// function createBookBlock(book, index) {
-//     const bookDiv = document.createElement('div');
-//     bookDiv.classList.add('book');
-//     bookDiv.setAttribute('data-attribute', index);
-//     bookDiv.innerHTML = `<h2>Title: ${book.title}</h2>
-//         <p><em>Author:</em> ${book.author}</p>
-//         <p><em>Pages:</em> ${book.pages}</p>
-//         <p><em>Read:</em> ${book.read ? 'Yes' : 'No'}</p>
-//         <button class="remove" onclick='removeBook(${index})'>Remove</button>
-//         <label>Have you read it?</label>`;
-
-//     const readCheckbox = document.createElement('input');
-
-//     readCheckbox.type = 'checkbox';
-//     readCheckbox.classList.add('read');
-
-//     if (book.read) {
-//         bookDiv.style.backgroundColor = 'lightgreen';
-//         readCheckbox.checked = true;
-//     }
-
-//     readCheckbox.addEventListener('change', () => {
-//         if (readCheckbox.checked) {
-//             bookDiv.style.backgroundColor = 'lightgreen';
-//         }
-//         else {
-//             bookDiv.style.backgroundColor = 'white';
-//         }
-
-//     });
-
-//     bookDiv.appendChild(readCheckbox);
-//     libraryDiv.appendChild(bookDiv);
-// }
-
-// const library = new Library();
-// displayLibrary();
-
 class ScreenController {
     addButton = document.querySelector('button');
     authorInput = document.querySelector('#author');
@@ -148,44 +33,6 @@ class ScreenController {
         this.addButton.addEventListener('click', this.addBook.bind(this));
         this.library = new Library();
     }
-
-    // displayLibrary() {
-
-    //     this.library.books.forEach((book, index) => {
-    //         const bookDiv = document.createElement('div');
-    //         bookDiv.classList.add('book');
-    //         bookDiv.setAttribute('data-attribute', index);
-    //         bookDiv.innerHTML = `<h2>Title: ${book.title}</h2>
-    //         <p><em>Author:</em> ${book.author}</p>
-    //         <p><em>Pages:</em> ${book.pages}</p>
-    //         <p><em>Read:</em> ${book.read ? 'Yes' : 'No'}</p>
-    //         <button class="remove" onclick='removeBook(${index})'>Remove</button>
-    //         <label>Have you read it?</label>`;
-
-    //         const readCheckbox = document.createElement('input');
-
-    //         readCheckbox.type = 'checkbox';
-    //         readCheckbox.classList.add('read');
-
-    //         if (book.read) {
-    //             bookDiv.style.backgroundColor = 'lightgreen';
-    //             readCheckbox.checked = true;
-    //         }
-
-    //         readCheckbox.addEventListener('change', () => {
-    //             if (readCheckbox.checked) {
-    //                 bookDiv.style.backgroundColor = 'lightgreen';
-    //             }
-    //             else {
-    //                 bookDiv.style.backgroundColor = 'white';
-    //             }
-
-    //         });
-
-    //         bookDiv.appendChild(readCheckbox);
-    //         this.libraryDiv.appendChild(bookDiv);
-    //     });
-    // }
 
     addBook(event) {
 
