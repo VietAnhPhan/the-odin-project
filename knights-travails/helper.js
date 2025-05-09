03791b7ec9c4db21979afb89924355325230dae0
Anh Phan <vietanhphan2810@gmail.com>
2025-05-09 04:11:41 +0000

    replace range of 1 to n by 0 to n-1 of checking isInside function
    The index of range start at 0 at end at n-1

--- knights-travails/helper.js
+++ knights-travails/helper.js
@@ -1,6 +1,6 @@
 export class helper {
   static boardSize = 8;
   static isInside(x, y) {
-    return x >= 1 && x <= helper.boardSize && y >= 1 && y <= helper.boardSize;
+    return x >= 0 && x < helper.boardSize && y >= 0 && y < helper.boardSize;
   }
 }
